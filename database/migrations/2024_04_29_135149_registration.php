4813bafe2f13b9689e7cdf6e09dce85cb668ab94
ChandraTERA <Chandrawpsiloto@gmail.com>
2024-07-15 15:37:35 +0700

    membuat table registrasion setiap user yang ingin mendaftar data tersimpan di database

---
+++ database/migrations/2024_04_29_135149_registration.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('registrasion', static function (Blueprint $table){
+            $table->id();
+            $table->string('username');
+            $table->string('email')->unique();
+            // $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            // $table->rememberToken();
+            // $table->timestamp('waktu_sekarang')->userCurrent();
+            $table->timestamps();
+        }); 
+        // Schema::create('login', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->string('email')->unique;
+        //     $table->string('password');
+        //     $table->timestamps();
+        //     $table->timestamp('waktu_sekarang')->useCurrent();
+        // });
+    }
+    
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
